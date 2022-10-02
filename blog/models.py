f3d06c09691e57eef017bfa1e2bd9f1610e17b0d
Michelle Hickey <michellehickey2@yahoo.ie>
2022-10-02 10:00:00 +0000

    Add post_details page to site

--- blog/models.py
+++ blog/models.py
@@ -6,13 +6,14 @@ from cloudinary.models import CloudinaryField
 STATUS = ((0, "Draft"), (1, "Published"))
 
 
+
 class Post(models.Model):
     title = models.CharField(max_length=200, unique=True)
     slug = models.SlugField(max_length=200, unique=True)
     author = models.ForeignKey(
         User, on_delete=models.CASCADE, related_name="blog_posts"
     )
-    featured_image = CloudinaryField('image', default='placeholder')
+    featured_image = CloudinaryField('image', default='placeholder')    
     excerpt = models.TextField(blank=True)
     updated_on = models.DateTimeField(auto_now=True)
     content = models.TextField()
@@ -26,6 +27,7 @@ class Post(models.Model):
 
     def __str__(self):
         return self.title
+      
 
     def number_of_likes(self):
         return self.likes.count()
