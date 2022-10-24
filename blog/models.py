b114b2f6e90395889d47819ac1ad435eafd31f22
Michelle Hickey <michellehickey2@yahoo.ie>
2022-10-24 11:23:21 +0000

    Add number of comments to blogs and changed default image

--- blog/models.py
+++ blog/models.py
@@ -60,6 +60,9 @@ class Post(models.Model):
     def number_of_likes(self):
         return self.likes.count()
 
+    def number_of_comments(self):
+        return self.comments.count()
+
 
 class Comment(models.Model):
     post = models.ForeignKey(Post, on_delete=models.CASCADE,
@@ -75,6 +78,8 @@ class Comment(models.Model):
 
     def __str__(self):
         return f"Comment {self.body} by {self.name}"
+
+    
 
 
 class Photo(models.Model):
