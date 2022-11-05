6daaee2f781e0ef99bb6fcd278dd6215f7cda3ba
Michelle Hickey <michellehickey2@yahoo.ie>
2022-11-05 13:26:45 +0000

    Profile now working when user signs up and profile page showing

--- profiles/models.py
+++ profiles/models.py
@@ -1,29 +1,32 @@
 from django.db import models
 from django.contrib.auth.models import User
 from cloudinary.models import CloudinaryField
-# from django.db.models.signals import post_save
-# from django.dispatch import receiver
+from django.db.models.signals import post_save
 
 
 class Profile(models.Model):
     """
     Model for user profile
     """
-    user = models.OneToOneField(User, on_delete=models.CASCADE)
-    avatar = CloudinaryField('image', default='placeholder')   
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    avatar = CloudinaryField('image', default='placeholder')
     bio = models.TextField()
 
     def __str__(self):
         return f'{self.user.username} Profile'
+    
+    def create_profile(instance, created, **kwargs):
+        if created:
+            Profile.objects.create(user=instance)
 
 
-# @receiver(post_save, user)
-# def create_or_update_user_profile(user, instance, created, **kwargs):
-#     """
-#     Create or update the user profile
-#     """
-#     if created:
-#         UserProfile.objects.create(user=instance)
-#     # Existing users: just save the profile
-#     instance.userprofile.save()
+def create_profile(instance, created, **kwargs):
+    """
+    Create or update the user profile
+    """
+    if created:
+        Profile.objects.create(user=instance)
+    
+
+post_save.connect(create_profile, sender=User)
     
