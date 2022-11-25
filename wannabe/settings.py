62f29d6eec313a282cc12fe50f41513686429dab
Michelle Hickey <michellehickey2@yahoo.ie>
2022-11-25 10:54:59 +0000

    Testing debug false to check on deployment issues

--- wannabe/settings.py
+++ wannabe/settings.py
@@ -30,7 +30,8 @@ TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = 'DEV' in os.environ
+#DEBUG = 'DEV' in os.environ
+DEBUG = False
 
 ALLOWED_HOSTS = ['wannabe2022.herokuapp.com', 'localhost']
 
