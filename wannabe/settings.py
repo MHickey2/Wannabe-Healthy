29bc228b3cc9da2a8f7433f41705fc550cc773d8
Michelle Hickey <michellehickey2@yahoo.ie>
2023-09-12 15:58:18 +0100

    Update settings.py for adding render to allowed hosts

--- wannabe/settings.py
+++ wannabe/settings.py
@@ -35,7 +35,10 @@ DEBUG = False
 X_FRAME_OPTIONS = 'SAMEORIGIN'
 
 ALLOWED_HOSTS = ['wannabe2022.herokuapp.com', 'localhost']
-
+# Add Render.com URL to allowed hosts
+ RENDER_EXTERNAL_HOSTNAME = os.environ.get('RENDER_EXTERNAL_HOSTNAME')
+ if RENDER_EXTERNAL_HOSTNAME:
+   ALLOWED_HOSTS.append(RENDER_EXTERNAL_HOSTNAME)
 
 # Application definition
 
